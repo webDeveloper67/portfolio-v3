28d8f690a031c2f0c2371b15465db2e3db59f331
mdee <design4web2014@gmail.com>
2019-08-20 15:53:20 +0430

    initializing webpack with bootstrap and fancy box

---
+++ app/index.js
@@ -0,0 +1,15 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.scss';
+import '@fancyapps/fancybox/dist/jquery.fancybox.css';
+
+// ================================
+// START YOUR APP HERE
+// ================================
+import 'jquery';
+window.$ = $;
+import 'bootstrap';
+import '@fancyapps/fancybox/dist/jquery.fancybox.js';
