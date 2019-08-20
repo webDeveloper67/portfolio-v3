85fb3a93c81edb1330e582b6d5e4ad9a77c4345b
mdee <design4web2014@gmail.com>
2019-08-20 15:57:07 +0430

    adding font awesome scss files to the project

--- webpack.config.js
+++ webpack.config.js
@@ -88,6 +88,19 @@ module.exports = {
 					outputPath: 'imgs/'
 				}
 			},
+			// FONTS
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'fonts/'
+						}
+					}
+				]
+			},
 			//JQUERY
 			{
 				test: require.resolve('jquery'),
